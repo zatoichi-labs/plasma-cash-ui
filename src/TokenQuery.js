269787a7c930f4876f73c26a382e53367e6f2aa4
Bryant Eisenbach <fubuloubu@gmail.com>
2019-11-11 17:49:06 -0500

    refactor: Display plaintext name of token query result

--- src/TokenQuery.js
+++ src/TokenQuery.js
@@ -5,8 +5,14 @@ import { useSubstrate } from './substrate-lib';
 import { TxButton } from './substrate-lib/components';
 
 export default function TokenQuery (props) {
-  const { api } = useSubstrate();
+  const { api, keyring } = useSubstrate();
   const [status, setStatus] = useState(null);
+
+  // Get the list of accounts we possess the private key for
+  const accounts = keyring.getPairs().map(account => ({
+    address: account.address,
+    name: account.meta.name.toUpperCase(),
+  }));
 
   const initialState = {
     tokenId: '',
@@ -21,6 +27,25 @@ export default function TokenQuery (props) {
         [data.state]: data.value
       };
     });
+  };
+
+  const setQueryStatus = status => {
+    try {
+      let obj = JSON.parse(status);
+      if (obj.receiver) {
+        let receiver = accounts.filter(account => account.address === obj.receiver);
+        // If one and only one account is found with the matching address in our
+        // address book, display their name
+        if (receiver.length === 1) {
+          setStatus("Owner: " + receiver[0].name);
+        } else {
+          // else, display the address we've received
+          setStatus("Owner: " + obj.receiver);
+        }
+      }
+    } catch {
+      setStatus("Owner: Unknown!");
+    }
   };
 
   return (
@@ -41,7 +66,7 @@ export default function TokenQuery (props) {
         <Form.Field>
           <TxButton
             label='Query'
-            setStatus={setStatus}
+            setStatus={setQueryStatus}
             type='QUERY'
             attrs={{
               params: [tokenId],
